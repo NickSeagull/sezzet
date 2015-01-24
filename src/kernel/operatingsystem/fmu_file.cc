e59d6c3e0eb7ff4f3ffc0e2ff4c097a015486e40
Nikita Tchayka <nikitatchayka@gmail.com>
2015-01-24 13:25:28 +0000

    Add proper temporary folder name building

--- src/kernel/operatingsystem/fmu_file.cc
+++ src/kernel/operatingsystem/fmu_file.cc
@@ -1,11 +1,17 @@
 #include "fmu_file.h"
 #include <memory>
+#include <chrono>
 #include <boost/filesystem.hpp>
+#include <boost/lexical_cast.hpp>
 
-using std::make_shared;
 using boost::filesystem::temp_directory_path;
 using boost::filesystem::path;
 using boost::filesystem::create_directory;
+using boost::lexical_cast;
+using std::chrono::milliseconds;
+using std::chrono::duration_cast;
+using std::chrono::high_resolution_clock;
+using std::make_shared;
 
 FmuFile::FmuFile(string fmu_file_path) :
 	unzipper_(make_shared<MinizipUnzipper>(fmu_file_path)),
@@ -13,7 +19,11 @@ FmuFile::FmuFile(string fmu_file_path) :
 	working_directory_path_(MakeTemporalDirectory()){}
 
 string FmuFile::MakeTemporalDirectory(){
-	string temp_folder = temp_directory_path().string() + "/fmu";
+	string temp_folder =
+		temp_directory_path().string() +
+		"/fmu_" +
+		lexical_cast<string>(duration_cast<milliseconds> (high_resolution_clock::now().time_since_epoch()).count()) +
+		path(fmu_file_path_).stem().string();
 	create_directory(path(temp_folder));
 	return temp_folder;
 }
