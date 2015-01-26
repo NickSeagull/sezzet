5e045983722e4eed263f6bc611955bf44686d2d3
Nikita Tchayka <nikitatchayka@gmail.com>
2015-01-26 13:26:55 +0000

    Open and close zip when constructing/destructing

--- src/kernel/operatingsystem/libarchive_unzipper.cc
+++ src/kernel/operatingsystem/libarchive_unzipper.cc
@@ -1,6 +1,14 @@
 #include "libarchive_unzipper.h"
+#include <memory>
 
-LibArchiveUnzipper::LibArchiveUnzipper(string file_name){
+using std::make_shared;
+
+LibArchiveUnzipper::LibArchiveUnzipper(string file_name):
+	zip_file_(archive_read_new()){
 	file_name_ = file_name;
+}
+
+LibArchiveUnzipper::~LibArchiveUnzipper(){
+	archive_read_free(zip_file_);
 }
 
--- src/kernel/operatingsystem/libarchive_unzipper.h
+++ src/kernel/operatingsystem/libarchive_unzipper.h
@@ -1,11 +1,12 @@
 #ifndef KERNEL_OPERATINGSYSTEM_LIBARCHIVEUNZIPPER_H_
 #define KERNEL_OPERATINGSYSTEM_LIBARCHIVEUNZIPPER_H_
-#include <minizip/unzip.h>
+#include "../../../include/libarchive/archive.h"
 #include <string>
 #include "unzipper.h"
+#include <memory>
 
 using std::string;
-using std::ofstream;
+using std::shared_ptr;
 
 class LibArchiveUnzipper: public Unzipper {
 public:
@@ -13,6 +14,6 @@ public:
 	~LibArchiveUnzipper();
 	void ExtractTo(string output_path, string file_name);
 private:
-
+    archive *zip_file_;
 };
 #endif
