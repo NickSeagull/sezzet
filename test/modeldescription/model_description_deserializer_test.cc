92c376774a8ffa45d6ad20ca30e40a4264296215
Nikita Tchayka <nikitatchayka@gmail.com>
2015-02-04 13:34:24 +0000

    Add tests for deserializer

--- test/modeldescription/model_description_deserializer_test.cc
+++ test/modeldescription/model_description_deserializer_test.cc
@@ -3,18 +3,17 @@
 #define BOOST_TEST_MODULE ModelDescriptionDeserializerTest
 
 #include <boost/test/unit_test.hpp>
-#include <string>
-#include <memory>
 #include "model_description_test_fixture.cc"
 
-using std::string;
-using std::make_shared;
-
-BOOST_FIXTURE_TEST_CASE( it_is_possible_to_initialize_deserializer , Fixture){
-	BOOST_CHECK(deserializer != NULL);
+BOOST_FIXTURE_TEST_CASE( it_is_possible_to_initialize_deserializer , Fixture ){
+	BOOST_CHECK(deserializer != NULL); 
 }
 
-BOOST_FIXTURE_TEST_CASE( deserialized_model_description_is_not_null , Fixture){
+BOOST_FIXTURE_TEST_CASE( deserialized_model_description_is_not_null , Fixture ){
 	BOOST_CHECK(model_description != NULL);
 }
 
+BOOST_FIXTURE_TEST_CASE( deserializer_gets_the_fmi_version , Fixture ){
+	deserializer->Deserialize(model_description, "")
+		BOOST_CHECK(model_description->fmi_version() == "1.0");
+}
