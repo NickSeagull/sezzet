31415dae8ed818299bd7306248d9cfeef1ace194
Nikita Tchayka <nikitatchayka@gmail.com>
2015-02-23 10:13:03 +0000

    Fix a typo on can_be_instantiated_only_once_per...

--- src/modeldescription/v2/model_exchange.h
+++ src/modeldescription/v2/model_exchange.h
@@ -13,7 +13,7 @@ private:
 	string model_identifier_;
 	bool needs_execution_tool_;
 	bool completed_integrator_step_not_needed_;
-	bool can_be_instantiated_only_per_process_;
+	bool can_be_instantiated_only_once_per_process_;
 	bool can_not_use_memory_management_functions_;
 	bool can_get_and_set_fmu_state_;
 	bool can_serialize_fmu_state_;
@@ -29,8 +29,8 @@ public:
 	bool can_get_and_set_fmu_state();
 	void can_not_use_memory_management_functions(bool new_can_not_use_memory_management_functions);
 	bool can_not_use_memory_management_functions();
-	void can_be_instantiated_only_per_process(bool new_can_be_instantiated_only_per_process);
-	bool can_be_instantiated_only_per_process();
+	void can_be_instantiated_only_once_per_process(bool new_can_be_instantiated_only_once_per_process);
+	bool can_be_instantiated_only_once_per_process();
 	void completed_integrator_step_not_needed(bool new_completed_integrator_step_not_needed);
 	bool completed_integrator_step_not_needed();
 	void needs_execution_tool(bool new_needs_execution_tool);
