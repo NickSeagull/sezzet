be59ccd34965d12e829c6681fb92230f1f1beb98
Nikita Tchayka <nikitatchayka@gmail.com>
2015-02-25 11:47:25 +0000

    Remove inlines

--- src/modeldescription/v2/default_experiment.h
+++ src/modeldescription/v2/default_experiment.h
@@ -8,14 +8,14 @@ private:
 	double stop_time_;
 	double step_size_;
 public:
-	inline void step_size(double new_step_size);
-	inline double step_size();
-	inline void stop_time(double new_stop_time);
-	inline double stop_time();
-	inline void start_time(double new_start_time);
-	inline double start_time();
-	inline void tolerance(double new_tolerance);
-	inline double tolerance();
+	void step_size(double new_step_size);
+	double step_size();
+	void stop_time(double new_stop_time);
+	double stop_time();
+	void start_time(double new_start_time);
+	double start_time();
+	void tolerance(double new_tolerance);
+	double tolerance();
     DefaultExperiment();
 };
 
