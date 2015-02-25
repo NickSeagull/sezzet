11e12918ab3c7ac5ff151342e76926366087a3f0
Nikita Tchayka <nikitatchayka@gmail.com>
2015-02-25 13:42:54 +0000

    Add BooleanTypeFiller

---
+++ src/modeldescription/elementtree/boolean_type_filler.cc
@@ -0,0 +1,13 @@
+#include "boolean_type_filler.h"
+
+BooleanTypeFiller::BooleanTypeFiller(){}
+
+void BooleanTypeFiller::Fill(BooleanType& boolean_type, shared_ptr<Node> node){
+	for(auto& pair : node->attributes())
+		SetField(boolean_type, pair.first, pair.second);
+}
+
+void BooleanTypeFiller::SetField(BooleanType& boolean_type, string field_name, string field_value){
+	if(field_name == "declaredType") boolean_type.declared_type(field_value);
+	if(field_name == "start") boolean_type.declared_type(field_value == "true");
+}
