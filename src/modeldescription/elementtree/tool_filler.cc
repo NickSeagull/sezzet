7c3808137ae6e51265a32fc58c37fdd9fffe15c5
Nikita Tchayka <nikitatchayka@gmail.com>
2015-02-25 13:34:19 +0000

    Add ToolFiller

---
+++ src/modeldescription/elementtree/tool_filler.cc
@@ -0,0 +1,12 @@
+#include "tool_filler.h"
+
+ToolFiller::ToolFiller(){}
+
+void ToolFiller::Fill(Tool& tool, shared_ptr<Node> node){
+	for(auto& pair : node->attributes())
+		SetField(tool, pair.first, pair.second);
+}
+
+void ToolFiller::SetField(Tool& tool, string field_name, string field_value){
+	if(field_name == "name") tool.name(field_value);
+}
