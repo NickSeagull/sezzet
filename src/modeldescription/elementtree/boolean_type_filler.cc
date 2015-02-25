8eb8decf979bf9995bc66d2a4835c5f167b63f5e
Nikita Tchayka <nikitatchayka@gmail.com>
2015-02-25 14:25:21 +0000

    Fix

--- src/modeldescription/elementtree/boolean_type_filler.cc
+++ src/modeldescription/elementtree/boolean_type_filler.cc
@@ -9,5 +9,5 @@ void BooleanTypeFiller::Fill(BooleanType& boolean_type, shared_ptr<Node> node){
 
 void BooleanTypeFiller::SetField(BooleanType& boolean_type, string field_name, string field_value){
 	if(field_name == "declaredType") boolean_type.declared_type(field_value);
-	if(field_name == "start") boolean_type.declared_type(field_value == "true");
+	if(field_name == "start") boolean_type.start(field_value == "true");
 }
