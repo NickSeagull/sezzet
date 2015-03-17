c4f88c0ea728e4b711cf5c793dd198936bac9af8
Nikita Tchayka <nikitatchayka@gmail.com>
2015-03-17 11:05:51 +0000

    Refactor ExtractTo

--- src/kernel/operatingsystem/SevenZipUnzipper.cpp
+++ src/kernel/operatingsystem/SevenZipUnzipper.cpp
@@ -31,16 +31,31 @@ void InitalizeProcessStructs(STARTUPINFO &startup_info, PROCESS_INFORMATION &pro
 	ZeroMemory(&process_info, sizeof(process_info));
 }
 
-void SevenZipUnzipper::ExtractTo(string output_path) {
-#ifdef _WIN32
-	STARTUPINFO startup_info;
-	PROCESS_INFORMATION process_info;
-	string command = "7z x " + file_path_ + " -o\"" + output_path + "\" -y";
-	InitalizeProcessStructs(startup_info, process_info);
-	if (!CreateNewProcess(command, startup_info, process_info)) throw std::exception("Could not execute 7zip");
+
+void CloseProcess(PROCESS_INFORMATION &process_info) {
 	WaitForSingleObject(process_info.hProcess, INFINITE);
 	CloseHandle(process_info.hProcess);
 	CloseHandle(process_info.hThread);
+}
+
+
+void Do7zipExctraction(STARTUPINFO &startup_info, PROCESS_INFORMATION &process_info, string command) {
+	InitalizeProcessStructs(startup_info, process_info);
+	if (!CreateNewProcess(command, startup_info, process_info)) throw std::exception("Could not execute 7zip");
+	CloseProcess(process_info);
+}
+
+
+void ExecuteWindowsExtraction(string file_path_, string output_path) {
+	STARTUPINFO startup_info;
+	PROCESS_INFORMATION process_info;
+	string command = "7z x " + file_path_ + " -o\"" + output_path + "\" -y";
+	Do7zipExctraction(startup_info, process_info, command);
+}
+
+void SevenZipUnzipper::ExtractTo(string output_path) {
+#ifdef _WIN32
+	ExecuteWindowsExtraction(file_path_, output_path);
 #else
 	throw exception("Operating system not supported!");
 #endif
