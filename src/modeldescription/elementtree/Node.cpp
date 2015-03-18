77cb46e9dce68d6abc5f512906114eeff9eda29b
Nikita Tchayka <nikitatchayka@gmail.com>
2015-03-18 11:13:28 +0000

    Refactor

--- src/modeldescription/elementtree/Node.cpp
+++ src/modeldescription/elementtree/Node.cpp
@@ -3,11 +3,7 @@
 using std::make_shared;
 
 Node::Node(){}
-
-Node::~Node() {
-	childs_.clear();
-	attributes_.clear();
-}
+Node::~Node() {}
 
 unordered_map<string, string> Node::attributes() { return attributes_; }
 void Node::attributes(unordered_map<string, string> new_attributes){attributes_ = new_attributes;}
@@ -23,10 +19,8 @@ void Node::AddAttribute(string attribute_name, string value) {
 
 string Node::GetAttribute(string attribute_name){
 	auto result = attributes_.find(attribute_name);
-	if (result != attributes_.end())
-		return result->second;
-	else
-		return "Not Found";
+	if (result == attributes_.end()) return "Not Found";
+	return result->second;
 }
 
 void Node::AddChild(shared_ptr<Node> child){
